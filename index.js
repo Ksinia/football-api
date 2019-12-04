1703098c57fa2849b6ef0fa043e53e77a96764d6
Ksinia <xiatian@mail.ru>
2019-12-04 15:37:39 +0100

    Add POST method for adding new teams

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 4000;
 
-app.listen(port, console.log(`Listening on :${port}`));
+const app = express();
+
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
 
 const teamRouter = require("./team/router");
 app.use(teamRouter);
+
+const port = process.env.PORT || 4000;
+app.listen(port, console.log(`Listening on :${port}`));
--- team/router.js
+++ team/router.js
@@ -6,5 +6,10 @@ router.get("/team", (req, res, next) => {
     .then(data => res.send(data))
     .catch(errors => next(errors));
 });
+router.post("/team", (req, res, next) => {
+  Team.create(req.body)
+    .then(team => res.send(team))
+    .catch(errors => next(errors));
+});
 
 module.exports = router;
